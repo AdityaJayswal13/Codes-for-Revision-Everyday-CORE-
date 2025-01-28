2ee169824180cc28c4ee3ee2397776629b944356
Aditya Jayswal <aj417650@gmail.com>
2025-01-28 14:55:13 +0530

    Update maximum-number-of-fish-in-a-grid.java

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,3 +1,13 @@
+//Here we want to find the maximum fish that can be found within and adjacent cells
+
+// so we use dfs
+// we take a m*n matrix initially with false values(visited) and check whether the value in the given matrix is >1 and (not visited) in visited matrix
+// if true then simply make the cell true and check the adjacent cells that have  not true and >1 conditions
+// for the adjacent cells we take a array dir=-1,0,1,0,-1
+// and take a for loop to initalise from 0 to 4 and for each newX and newY just check it is in valid indices and not visited and and value is >0
+// and recursively call for another value and return it 
+// and lastly find the maximum value out of the values
+
 class Solution {
     int[] dir={-1,0,1,0,-1};
     public boolean isValid(int x,int y,int m,int n){
