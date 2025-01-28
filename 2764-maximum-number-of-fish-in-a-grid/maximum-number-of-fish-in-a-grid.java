930d48e657dc92b1e5d270f00e619670b87701ac
Aditya Jayswal <aj417650@gmail.com>
2025-01-28 15:02:24 +0530

    Update maximum-number-of-fish-in-a-grid.java

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -11,16 +11,16 @@
 class Solution {
     int[] dir={-1,0,1,0,-1};
     public boolean isValid(int x,int y,int m,int n){
-        return (x>=0 && x<m && y>=0 && y<n);
+        return (x>=0 && x<m && y>=0 && y<n);//check whether any one of them is out of bound or not
     }
     public int dfs(int[][] grid,boolean[][] visited ,int m, int n,int x,int y){
-        int fish_count=grid[x][y];
-        for(int i=0;i<4;i++){
+        int fish_count=grid[x][y];//it is > 1 always because during function call it should be >1
+        for(int i=0;i<4;i++){//it check four adjacent directions
             int newX=x+dir[i];
             int newY=y+dir[i+1];
             if(isValid(newX,newY,m,n) && !visited[newX][newY] && grid[newX][newY]>0){
                 visited[newX][newY]=true;
-                fish_count+=dfs(grid,visited,m,n,newX,newY);
+                fish_count+=dfs(grid,visited,m,n,newX,newY);//recursive call for next cells for new x and y
             }
         }
         return fish_count;
@@ -30,7 +30,7 @@ class Solution {
         int n=grid[0].length;
         int max_fish=0;
         boolean[][] visited=new boolean[m][n];
-        
+        //going to every cell and check if the value is >1 and visited cell is false or not
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(!visited[i][j] && grid[i][j]>0){
