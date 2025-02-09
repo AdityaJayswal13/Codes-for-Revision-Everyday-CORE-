af1bcfa1dc81aefb486089678bf5afa81d905700
Aditya Jayswal <aj417650@gmail.com>
2025-02-09 15:18:06 +0530

    Update merge-sorted-array.java

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,3 +1,10 @@
+//here the logic is simply backtracking
+//we simply take the last element of the two arrays and comparing with themseleve whosever small just put it to the last of nums1 because it has extra space
+//now one very important logic what happen when the one array is empty then i=-1 as per the code 
+//so remember we give i>=0 condition inside the while loop so that if array is empty the iteration go to the else part
+
+
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
        int i=m-1;
