f6d7684ad723bef5ce0fb0125a8dcccfc6a4fee6
Aditya Jayswal <aj417650@gmail.com>
2025-02-10 11:03:23 +0530

    Update kth-largest-element-in-an-array.java

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,3 +1,6 @@
+//simply put every element in priority Queue of descending order tyype and remove until k-1 and peek the kth largest.
+
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq=new PriorityQueue(Collections.reverseOrder());
