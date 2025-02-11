445058c867e29817e47d3c20197334d8bc3b3b6a
Aditya Jayswal <aj417650@gmail.com>
2025-02-11 07:33:37 +0530

    Update diameter-of-binary-tree.java

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,6 +13,16 @@
  *     }
  * }
  */
+//Logic : to find diameter we have two cases first is that the diameter passes through root and second cases it is from any of the subtree
+// So what we done here is
+//first of all we take a Info class to store the height and diameter of every node in the tree
+//why height because to find the length of the tree in which the root of the tree is a part
+//So simply call for leftInfo and rightInfo of the root.left and root.right
+//and simply find the diameter and height after the info
+// return the info with diameter and height
+//after finding the info object with diameter and height then we simply return the diameter
+
+
  class Info{
         int diam;
         int ht;
