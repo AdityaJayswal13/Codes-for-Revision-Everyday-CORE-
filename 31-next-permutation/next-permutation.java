9960ed553ba1166b3c520e2faa9e44c5c2be92db
Aditya Jayswal <aj417650@gmail.com>
2025-06-24 22:39:49 +0530

    Time: 1 ms (14.88%) | Memory: 43.4 MB (11.50%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,36 +1,29 @@
-//for finding next permutation
-//since we observe that if we iterate from end we find that there is a values in descending order and at a point we find the end of descending
-//value so at the point were we find the end of descending value we get pivot point
-//after finding pivot find we iterate again from last until we find the number which is greater than pivot index value 
-//then simply swap it with the greater value
-//and last sort the value after pivot index so that we get the next permutation
-
 class Solution {
-    public void swap(int arr[],int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    public void swap(int[] arr,int a,int b){
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
     }
-    public void nextPermutation(int[] arr) {
-        int n=arr.length;
+    public void nextPermutation(int[] nums) {
         int pivot=-1;
+        int n=nums.length;
         for(int i=n-1;i>0;i--){
-            if(arr[i]>arr[i-1]){
+            if(nums[i]>nums[i-1]){
                 pivot=i-1;
                 break;
             }
         }
         if(pivot==-1){
-            Arrays.sort(arr);
+            Arrays.sort(nums);
         }
         else{
             for(int i=n-1;i>pivot;i--){
-                if(arr[i]>arr[pivot]){
-                    swap(arr,i,pivot);
+                if(nums[i]>nums[pivot]){
+                    swap(nums,i,pivot);
                     break;
                 }
             }
+            Arrays.sort(nums,pivot+1,n);
         }
-        Arrays.sort(arr,pivot+1,n) ;       
     }
 }
