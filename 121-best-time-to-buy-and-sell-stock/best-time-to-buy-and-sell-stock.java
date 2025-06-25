ebcd2b56f432bb73a624792942a4b0f960a1d7c4
Aditya Jayswal <aj417650@gmail.com>
2025-06-25 19:10:42 +0530

    Time: 1 ms (99.89%) | Memory: 61.7 MB (64.16%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,14 @@
-//just take three variable profit ,buyPrice intialize with Integer.maxvalue ,maxprofit
-//Remember if prices value is smaller than buyprice simply buyprice=prices[i]
-//if the prices are greater than buyprice simply find the profit 
-//and find the maxProfit
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        int buyPrice=Integer.MAX_VALUE;
+        int buyprice=Integer.MAX_VALUE;
         int maxProfit=0;
-        int l=prices.length;
-        for(int i=0;i<l;i++){
-            if(prices[i]<buyPrice){
-                buyPrice=prices[i];
+        
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<buyprice){
+                buyprice=prices[i];
             }
             else{
-                profit=prices[i]-buyPrice;
+                int profit=prices[i]-buyprice;
                 maxProfit=Math.max(profit,maxProfit);
             }
         }
